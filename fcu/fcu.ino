4fbea5d4f9a4ffabcca7dde1752e45774002728f
Alvaro Polo <apoloval@gmail.com>
2012-07-28 13:11:18 +0200

    Include declarations to handle FCU button state.

--- fcu/fcu.ino
+++ fcu/fcu.ino
@@ -5,6 +5,27 @@ word status;
 unsigned long selectedSpeed;
 unsigned long selectedHeading;
 
+word buttonState;
+
+enum ButtonStateMask
+{
+   BTN_FD       = 0x0001,
+   BTN_ILS      = 0x0002,
+   BTN_LOC      = 0x0004,
+   BTN_AP1      = 0x0008,
+   BTN_AP2      = 0x0010,
+   BTN_ATHR     = 0x0020,
+   BTN_EXP      = 0x0040,
+   BTN_APPR     = 0x0080,
+   BTN_SEL_SPD  = 0x0100,
+   BTN_SEL_HDG  = 0x0200,
+   BTN_ALT      = 0x0400,
+   BTN_SEL_VS   = 0x0800,
+   BTN_SPD_DISP = 0x1000,
+   BTN_HDG_DISP = 0x2000,
+   BTN_VS_DISP  = 0x4000,
+};
+
 ICM7218 leftDisplayController;
 ICM7218::DisplayGroup speedDisplayGroup;
 ICM7218::DisplayGroup headingDisplayGroup;
@@ -31,14 +52,12 @@ void setHeading(word value, bool selected = true)
 
 void setStatus(word value)
 {
-   if (status != value)
-   {
-      status = value;
+   status = value;
       
-      setSpeed(selectedSpeed, status & MASK_FCU_SPD_MOD);
-      
-      // TODO: update the rest of displays
-   }
+   setSpeed(selectedSpeed, status & MASK_FCU_SPD_MOD);
+   setHeading(selectedHeading, status & MASK_FCU_HDG_MOD);
+   
+   // TODO: update the rest of displays
 }
 
 void writeVariable(struct WriteVarCommand& cmd)
@@ -60,10 +79,19 @@ void writeVariable(struct WriteVarCommand& cmd)
    }
 }
 
+void reset()
+{
+   buttonState = 0;
+   setStatus(0);
+}
+
 void processCommand(union Command& cmd)
 {
    switch (cmd.type)
    {
+      case CMD_RESET:
+         reset();
+         break;
       case CMD_WRITE_VAR:
          writeVariable(cmd.writeVar);
          break;
@@ -71,12 +99,6 @@ void processCommand(union Command& cmd)
          Serial.print("Unknown command code ");
          Serial.println(cmd.type, HEX);
    }
-}
-
-void reset()
-{
-   setSpeed(0, false);
-   setHeading(0, false);
 }
 
 void setup()
