e2c326032162f89a3e3a7be796c9ba42e07a45c8
Alvaro Polo <apoloval@gmail.com>
2012-07-28 12:17:07 +0200

    Implement write var for FCU status.

--- fcu/fcu.ino
+++ fcu/fcu.ino
@@ -1,8 +1,9 @@
 #include "icm7218.h"
 #include "oacsp.h"
 
-unsigned long speedValue;
-unsigned long headingValue;
+word status;
+unsigned long selectedSpeed;
+unsigned long selectedHeading;
 
 ICM7218 leftDisplayController;
 ICM7218::DisplayGroup speedDisplayGroup;
@@ -10,75 +11,100 @@ ICM7218::DisplayGroup headingDisplayGroup;
 
 Command cmd;
 
-void setSpeed(word value)
+void setSpeed(word value, bool selected = true)
 {
-  speedValue = value;
-  speedDisplayGroup.displayNumber(speedValue);
+   selectedSpeed = value;
+   if (selected)
+      speedDisplayGroup.displayNumber(selectedSpeed);
+   else
+      speedDisplayGroup.displayDash();
 }
 
-void setHeading(word value)
+void setHeading(word value, bool selected = true)
 {
-  headingValue = value;
-  headingDisplayGroup.displayNumber(headingValue);
+   selectedHeading = value;
+   if (selected)
+      headingDisplayGroup.displayNumber(selectedHeading);
+   else
+      headingDisplayGroup.displayDash();
+}
+
+void setStatus(word value)
+{
+   if (status != value)
+   {
+      status = value;
+      
+      setSpeed(selectedSpeed, status & MASK_FCU_SPD_MOD);
+      
+      // TODO: update the rest of displays
+   }
 }
 
 void writeVariable(struct WriteVarCommand& cmd)
 {
-  switch (cmd.offset)
-  {
-    case VAR_FCU_SEL_SPD:
-      setSpeed(cmd.data);
-      break;
-    case VAR_FCU_SEL_HDG:
-      setHeading(cmd.data);
-      break;
-    default:
-      Serial.print("Invalid offset ");
-      Serial.println(cmd.offset, HEX);
-  }
+   switch (cmd.offset)
+   {
+      case VAR_FCU_STATUS:
+         setStatus(cmd.data);
+         break;
+      case VAR_FCU_SEL_SPD:
+         setSpeed(cmd.data);
+         break;
+      case VAR_FCU_SEL_HDG:
+         setHeading(cmd.data);
+         break;
+      default:
+         Serial.print("Invalid offset ");
+         Serial.println(cmd.offset, HEX);
+   }
 }
 
 void processCommand(union Command& cmd)
 {
-  switch (cmd.type)
-  {
-    case CMD_WRITE_VAR:
-      writeVariable(cmd.writeVar);
-      break;
-    default:
-      Serial.print("Unknown command code ");
-      Serial.println(cmd.type, HEX);
-  }
+   switch (cmd.type)
+   {
+      case CMD_WRITE_VAR:
+         writeVariable(cmd.writeVar);
+         break;
+      default:
+         Serial.print("Unknown command code ");
+         Serial.println(cmd.type, HEX);
+   }
+}
+
+void reset()
+{
+   setSpeed(0, false);
+   setHeading(0, false);
 }
 
 void setup()
 {
-  int idPins[] = { 2, 3, 4, 5, 6, 7, 8, 9 };
-  leftDisplayController.setWritePin(11);
-  leftDisplayController.setModePin(10);
-  leftDisplayController.setIdPins(idPins);
+   int idPins[] = { 2, 3, 4, 5, 6, 7, 8, 9 };
+   leftDisplayController.setWritePin(11);
+   leftDisplayController.setModePin(10);
+   leftDisplayController.setIdPins(idPins);
   
-  int speedDisplays[] = { 0, 1, 2, -1, -1, -1, -1, -1 };
-  speedDisplayGroup.setParent(&leftDisplayController);
-  speedDisplayGroup.setDisplays(speedDisplays);
+   int speedDisplays[] = { 0, 1, 2, -1, -1, -1, -1, -1 };
+   speedDisplayGroup.setParent(&leftDisplayController);
+   speedDisplayGroup.setDisplays(speedDisplays);
 
-  int headingDisplays[] = { -1, -1, -1, 0, 1, 2, -1, -1 };
-  headingDisplayGroup.setParent(&leftDisplayController);
-  headingDisplayGroup.setDisplays(headingDisplays);
+   int headingDisplays[] = { -1, -1, -1, 0, 1, 2, -1, -1 };
+   headingDisplayGroup.setParent(&leftDisplayController);
+   headingDisplayGroup.setDisplays(headingDisplays);
+
+   reset();  
   
-  setSpeed(0);
-  setHeading(0);
-  
-  Serial.begin(9600);
+   Serial.begin(9600);
 }
 
 void loop()
 {
-  if (Serial.available() >= sizeof(Command))
-  {
-    Serial.readBytes((char*) &cmd, sizeof(Command));
-    processCommand(cmd);
-  }
+   if (Serial.available() >= sizeof(Command))
+   {
+      Serial.readBytes((char*) &cmd, sizeof(Command));
+      processCommand(cmd);
+   }
 }
-
 
